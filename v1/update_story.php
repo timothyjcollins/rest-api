5011cb7a02b93fec43851847c35aac55ff4c9cdd
timothyjcollins <tcollins@greatergood.com>
2015-10-12 13:55:25 -0700

    update

--- v1/update_story.php
+++ v1/update_story.php
@@ -1,5 +1,5 @@
 <form action="http://52.89.200.46/v1/campaigns.php" method="POST">
-	<input type="hidden" name="apikey" value="12345">
+	<input type="hidden" name="apikey" value="123456">
 	<input type="hidden" name="request" value="update_story">
 	<input type="hidden" name="story_id" value="1">
 	<input name="email" size="40"><br>
