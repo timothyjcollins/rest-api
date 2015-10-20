2e984baad2e1da7ce884237ac595c074d5e4be5b
timothyjcollins <tcollins@greatergood.com>
2015-10-20 13:52:27 -0700

    update

--- v1/campaigns.php
+++ v1/campaigns.php
@@ -17,7 +17,7 @@
 					require 'aws/aws-autoloader.php';
 					$s3 = new AmazonS3();
 					$bucket = 'userstoriesimages' . strtolower($s3->key);
-					$result = $this -> s3Client -> putObject(array('Bucket' => $bucket, 'Key' => $_FILES["fileToUpload"], 'SourceFile' => $_FILES["fileToUpload"], 'ACL' => 'public-read'));
+					$result = $s3->putObject(array('Bucket' => $bucket, 'Key' => $_FILES["fileToUpload"], 'SourceFile' => $_FILES["fileToUpload"], 'ACL' => 'public-read'));
 			        $filename = $target_file;
 			        $uploadOk = 1;
 				}
