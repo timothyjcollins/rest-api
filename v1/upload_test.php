67e4a7ab31ec8080641d72c299a18ae6059f990b
timothyjcollins <tcollins@greatergood.com>
2015-10-20 09:34:18 -0700

    update

--- v1/upload_test.php
+++ v1/upload_test.php
@@ -5,6 +5,7 @@
 <form action="campaigns.php" method="post" enctype="multipart/form-data">
 	<input type="hidden" name="request" value="submit_story">
 	<input type="hidden" name="camid" value="88">
+	<input type="hidden" name="apikey" value="123456">
 	<input type="hidden" name="title" value="submit_story TEST">
 	<input type="hidden" name="desc" value="submit_story DESC">
 	
