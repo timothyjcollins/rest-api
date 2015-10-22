70ef57fee50ce1e92d97eefb400a0e705f014d93
timothyjcollins <tcollins@greatergood.com>
2015-10-22 07:07:10 -0700

    update

--- v1/campaigns.php
+++ v1/campaigns.php
@@ -20,12 +20,12 @@ $sharedConfig = [
     'version' => 'latest'
 ];
 $sdk = new Aws\Sdk($sharedConfig);					
-$s3Client = $sdk->createS3();
-$result = $s3Client->putObject([
-    'Bucket' => 'userstoriesimages',
-    'Key'    => 'console.aws.amazon.com/s3/home?region=us-west-2',
-    'Body'   => $_FILES["fileToUpload"]
-]);
+//$s3Client = $sdk->createS3();
+//$result = $s3Client->putObject([
+//    'Bucket' => 'userstoriesimages',
+//    'Key'    => 'console.aws.amazon.com/s3/home?region=us-west-2',
+//    'Body'   => $_FILES["fileToUpload"]
+//]);
 			        $filename = $target_file;
 			        $uploadOk = 1;
 				}
