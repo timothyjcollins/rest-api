46bf2baf4ff22a00cbb24aa2ddb39f713bbab09b
timothyjcollins <tcollins@greatergood.com>
2015-10-22 07:30:54 -0700

    update

--- v1/campaigns.php
+++ v1/campaigns.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'campaignapi.php';
-	require 'aws.phar';
+//require 'aws.phar';
 	// Requests from the same server don't have a HTTP_ORIGIN header
 	if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
 	    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
