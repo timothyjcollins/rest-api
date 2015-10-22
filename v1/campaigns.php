c9e678fc0808a99f3445b6acf3e0eb0c576aae38
timothyjcollins <tcollins@greatergood.com>
2015-10-22 07:13:26 -0700

    update

--- v1/campaigns.php
+++ v1/campaigns.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'campaignapi.php';
-	require_once 'aws/aws-autoloader.php';
+	require_once '/v1/aws/aws-autoloader.php';
 	// Requests from the same server don't have a HTTP_ORIGIN header
 	if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
 	    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
